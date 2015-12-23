4b7c5fda63aa5060b5f4984117621f1b3d5ff572
jjsub <jsamesub@gmail.com>
2015-12-23 16:40:57 -0600

    clock ticking and 0 if 9 < val added

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,28 @@
 function onReady(){
-  console.log('Hola todos');
+  console.log('Hola Todos');
+  
+  updateClock();
+  setInterval(updateClock, 1000);
 
+}       
+
+//Make the clock tick 
+function updateClock(){
   var date = new Date(); // This is invoquin the Date object construct by using 'new'
   var clock = document.getElementById('clock');
           
   console.info(clock);
-  clock.innerHTML = date.getHours() + ':' + date.getMinutes() +':'+ date.getSeconds();
-}       
+  clock.innerHTML = formatDigits(date.getHours()) + ':' + 
+  formatDigits(date.getMinutes()) +':'+ formatDigits(date.getSeconds());
+}
+
+//add a 0 if the value is smaller than 9 
+function formatDigits(val){
+  if(val < 10){
+     val = '0' + val;
+  }
+  return val;
+}
 
 var yea = '2015';
 
